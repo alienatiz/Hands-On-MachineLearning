2dcf684094ace515203984f848492c7966c86334
Byeongcheol Kim <kbc20000@gmail.com>
2022-02-23 18:34:22 +0900

    [2/2] Practice Chapter 6. Decision Tree
    * Add source code of decision_tree left
    * Add save_fig function in settings
    * min_samples_leaf_plot_r.png says the decision boundary which is generated with datasets rotated (It is different with min_samples_leaf_plot.png)
    
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- decision_tree.py
+++ decision_tree.py
@@ -1,7 +1,9 @@
 import graphviz
+import numpy as np
 from graphviz import Source
-from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
+from matplotlib.colors import ListedColormap
+from sklearn.datasets import load_iris, make_moons
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.tree import export_graphviz  # conda install python-graphviz
 
 from settings import *
@@ -18,25 +20,240 @@ export_graphviz(tree_clf)
 print(export_graphviz(tree_clf))
 
 export_graphviz(
-        tree_clf,
-        out_file=os.path.join(IMAGES_PATH, "iris_tree.dot"),
-        feature_names=iris.feature_names[2:],
-        class_names=iris.target_names,
-        rounded=True,
-        filled=True
+    tree_clf,
+    out_file=os.path.join(IMAGES_PATH, "iris_tree.dot"),
+    feature_names=iris.feature_names[2:],
+    class_names=iris.target_names,
+    rounded=True,
+    filled=True
 )
 
 Source.from_file(os.path.join(IMAGES_PATH, "iris_tree.dot"))
 
 with open("images/decision_trees/iris_tree.dot", encoding='UTF-8') as f:
-        dot_graph = f.read()
-        dot = graphviz.Source(dot_graph)
-        dot.format = 'png'
-        dot.render(filename='iris_tree', directory='images/decision_trees', cleanup=True)
+    dot_graph = f.read()
+dot = graphviz.Source(dot_graph)
+dot.format = 'png'
+dot.render(filename='iris_tree', directory='images/decision_trees', cleanup=True)
+dot
+
 
 # 6.2 Predict
+def plot_decision_boundary(clf, X, y, axes=[0, 7.5, 0, 3], iris=True, legend=False, plot_training=True):
+    x1s = np.linspace(axes[0], axes[1], 100)
+    x2s = np.linspace(axes[2], axes[3], 100)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    x_new = np.c_[x1.ravel(), x2.ravel()]
+    y_predict = clf.predict(x_new).reshape(x1.shape)
+    custom_colormap = ListedColormap(['#7d7d58', '#4c4c7f', '#507d50'])
+    plt.contourf(x1, x2, y_predict, alpha=0.3, cmap=custom_colormap)
+
+    if not iris:
+        custom_colormap2 = ListedColormap(['#7d7d58', '#4c4c7f', '#507d50'])
+        plt.contour(x1, x2, y_predict, cmap=custom_colormap2, alpha=0.8)
+    if plot_training:
+        plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "yo", label="Iris setosa")
+        plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "bs", label="Iris versicolor")
+        plt.plot(X[:, 0][y == 2], X[:, 1][y == 2], "g^", label="Iris virginica")
+        plt.axis(axes)
+    if iris:
+        plt.xlabel("Petal length", fontsize=14)
+        plt.ylabel("Petal width", fontsize=14)
+    else:
+        plt.xlabel(r"$x_1$", fontsize=14)
+        plt.ylabel(r"$x_2$", fontsize=14, rotation=0)
+    if legend:
+        plt.legend(loc="lower right", fontsize=14)
+
+
+plt.figure(figsize=(8, 4))
+plot_decision_boundary(tree_clf, X, y)
+plt.plot([2.45, 2.45], [0, 3], "k-", linewidth=2)
+plt.plot([2.45, 7.5], [1.75, 1.75], "k--", linewidth=2)
+plt.plot([4.95, 4.95], [0, 1.75], "k:", linewidth=2)
+plt.plot([4.85, 4.85], [1.75, 3], "k:", linewidth=2)
+plt.text(1.40, 1.0, "Depth=0", fontsize=15)
+plt.text(3.2, 1.80, "Depth=1", fontsize=13)
+plt.text(4.05, 0.5, "(Depth=2)", fontsize=11)
+save_fig("decision_tree_decision_boundaries_plot")
+plt.show()
+
 # 6.3 Estimate the probability of the class
 tree_clf.predict_proba([[5, 1.5]])
 print("tree_clf.predict_proba([[5, 1.5]]): \n", tree_clf.predict_proba([[5, 1.5]]))
 tree_clf.predict([[5, 1.5]])
 print("tree_clf.predict([[5, 1.5]]): \n", tree_clf.predict([[5, 1.5]]))
+
+# 6.4 CART Training Algorithm
+# Scikit-learn uses CART(Classification and regression tree) algorithm to train decision tree.
+# CART Algorithm is greedy algorithm. But finding the optimized tree is a NP-Complete problem.
+
+# 6.5 Time Complexity
+
+# 6.6 gini impurity or entropy?
+
+# 6.7 Regularization parameter
+tree_clf_tweaked = DecisionTreeClassifier(max_depth=2, random_state=40)
+tree_clf_tweaked.fit(X, y)
+
+plt.figure(figsize=(8, 4))
+plot_decision_boundary(tree_clf_tweaked, X, y, legend=False)
+plt.plot([0, 7.5], [0.8, 0.8], "k-", linewidth=2)
+plt.plot([0, 7.5], [1.75, 1.75], "k--", linewidth=2)
+plt.text(1.0, 0.9, "Depth=0", fontsize=15)
+plt.text(1.0, 1.80, "Depth=1", fontsize=13)
+save_fig("decision_tree_instability_plot")
+plt.show()
+
+# 6.7.1 Regularization using min_samples_leaf parameter
+Xm, ym = make_moons(n_samples=100, noise=0.25, random_state=53)
+
+deep_tree_clf1 = DecisionTreeClassifier(random_state=42)
+deep_tree_clf2 = DecisionTreeClassifier(min_samples_leaf=4, random_state=42)
+deep_tree_clf1.fit(Xm, ym)
+deep_tree_clf2.fit(Xm, ym)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+plt.sca(axes[0])
+plot_decision_boundary(deep_tree_clf1, Xm, ym, axes=[-1.5, 2.4, -1, 1.5], iris=False)
+plt.title("No restrictions", fontsize=16)
+plt.sca(axes[1])
+plot_decision_boundary(deep_tree_clf2, Xm, ym, axes=[-1.5, 2.4, -1, 1.5], iris=False)
+plt.title("min_samples_leaf = {}".format(deep_tree_clf2.min_samples_leaf), fontsize=14)
+plt.ylabel("")
+save_fig("min_samples_leaf_plot")
+plt.show()
+
+# 6.7.1.1 Rotate the datasets!
+angle = np.pi / 180 * 20
+rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+Xr = X.dot(rotation_matrix)
+
+tree_clf_r = DecisionTreeClassifier(random_state=42)
+tree_clf_r.fit(Xr, y)
+
+plt.figure(figsize=(8, 3))
+plot_decision_boundary(tree_clf_r, Xr, y, axes=[0.5, 7.5, -1.0, 1], iris=False)
+save_fig("min_samples_leaf_plot_r")
+plt.show()
+
+# 6.7.2 Decision tree which is sensitive to rotation of train set
+np.random.seed(6)
+Xs = np.random.rand(100, 2) - 0.5
+ys = (Xs[:, 0] > 0).astype(np.float32) * 2
+
+angle = np.pi / 4
+rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+Xsr = Xs.dot(rotation_matrix)
+
+tree_clf_s = DecisionTreeClassifier(random_state=42)
+tree_clf_s.fit(Xs, ys)
+tree_clf_sr = DecisionTreeClassifier(random_state=42)
+tree_clf_sr.fit(Xsr, ys)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+plt.sca(axes[0])
+plot_decision_boundary(tree_clf_s, Xs, ys, axes=[-0.7, 0.7, -0.7, 0.7], iris=False)
+plt.sca(axes[1])
+plot_decision_boundary(tree_clf_sr, Xsr, ys, axes=[-0.7, 0.7, -0.7, 0.7], iris=False)
+plt.ylabel("")
+save_fig("sensitivity_to_rotation_plot")
+plt.show()
+
+# 6.8 Regression
+np.random.seed(42)
+m = 200
+X = np.random.rand(m, 1)
+y = 4 * (X - 0.5) ** 2
+y = y + np.random.randn(m, 1) / 10
+
+tree_reg = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg.fit(X, y)
+
+# 6.8.1 Prediction of regression models of two decision trees
+tree_reg1 = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg2 = DecisionTreeRegressor(max_depth=3, random_state=42)
+tree_reg1.fit(X, y)
+tree_reg2.fit(X, y)
+
+
+def plot_regression_predictions(tree_reg, X, y, axes=[0, 1, -0.2, 1], ylabel="$y$"):
+    x1 = np.linspace(axes[0], axes[1], 500).reshape(-1, 1)
+    y_predict = tree_reg.predict(x1)
+    plt.axis(axes)
+    plt.xlabel("$x_1$", fontsize=18)
+    if ylabel:
+        plt.ylabel(ylabel, fontsize=18, rotation=0)
+    plt.plot(X, y, "b.")
+    plt.plot(x1, y_predict, "r.-", linewidth=2, label=r"$\hat{y}$")
+
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+plt.sca(axes[0])
+plot_regression_predictions(tree_reg1, X, y)
+for split, style in ((0.1973, "k-"), (0.0917, "k--"), (0.7718, "k--")):
+    plt.plot([split, split], [-0.2, 1], style, linewidth=2)
+plt.text(0.21, 0.65, "Depth=0", fontsize=15)
+plt.text(0.01, 0.2, "Depth=1", fontsize=13)
+plt.text(0.65, 0.8, "Depth=1", fontsize=13)
+plt.legend(loc="upper center", fontsize=18)
+plt.title("max_depth=2", fontsize=14)
+
+plt.sca(axes[1])
+plot_regression_predictions(tree_reg2, X, y, ylabel=None)
+for split, style in ((0.1973, "k-"), (0.0917, "k--"), (0.7718, "k--")):
+    plt.plot([split, split], [-0.2, 1], style, linewidth=2)
+for split in (0.0458, 0.1298, 0.2873, 0.9040):
+    plt.plot([split, split], [-0.2, 1], "k:", linewidth=1)
+plt.text(0.3, 0.5, "Depth=2", fontsize=13)
+plt.title("max_depth=3", fontsize=14)
+save_fig("tree_regression_plot")
+plt.show()
+
+# 6.8.2 Generate regression decision tree
+export_graphviz(
+    tree_reg1,
+    out_file=os.path.join(IMAGES_PATH, "regression_tree.dot"),
+    feature_names=["x1"],
+    rounded=True,
+    filled=True
+)
+Source.from_file(os.path.join(IMAGES_PATH, "regression_tree.dot"))
+
+with open("images/decision_trees/regression_tree.dot", encoding='UTF-8') as f:
+    dot_graph = f.read()
+dot = graphviz.Source(dot_graph)
+dot.format = 'png'
+dot.render(filename='regression_tree', directory='images/decision_trees', cleanup=True)
+dot
+
+# 6.8.3 Regulation of the regression model of decision tree
+tree_reg1 = DecisionTreeRegressor(random_state=42)
+tree_reg2 = DecisionTreeRegressor(min_samples_leaf=10, random_state=42)
+tree_reg1.fit(X, y)
+tree_reg2.fit(X, y)
+
+x1 = np.linspace(0, 1, 500).reshape(-1, 1)
+y_pred1 = tree_reg1.predict(x1)
+y_pred2 = tree_reg2.predict(x1)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+plt.sca(axes[0])
+plt.plot(X, y, "b.")
+plt.plot(x1, y_pred1, "r.-", linewidth=2, label=r"$\hat{y}$")
+plt.axis([0, 1, -0.2, 1.1])
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", fontsize=18, rotation=0)
+plt.legend(loc="upper center", fontsize=18)
+plt.title("No restrictions", fontsize=14)
+
+plt.sca(axes[1])
+plt.plot(X, y, "b.")
+plt.plot(x1, y_pred2, "r.-", linewidth=2, label=r"$\hat{y}$")
+plt.axis([0, 1, -0.2, 1.1])
+plt.xlabel("$x_1$", fontsize=18)
+plt.title("min_samples_leaf={}".format(tree_reg2.min_samples_leaf), fontsize=14)
+save_fig("tree_regression_regularization_plot")
+plt.show()
+
+# 6.9 Instability
--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
 
 import matplotlib
+import matplotlib.pyplot as plt
 
 # Setup matplotlib
 matplotlib.rcParams['axes.unicode_minus'] = False
@@ -9,3 +10,11 @@ PROJECT_ROOT_DIR = "."
 CHAPTER_ID = "decision_trees"
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
 os.makedirs(IMAGES_PATH, exist_ok=True)
+
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("그림 저장:", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
