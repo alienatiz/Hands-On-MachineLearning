0696315969fd2d0766b592a4c8fd251fe857a310
Byeongcheol Kim <kbc20000@gmail.com>
2022-03-08 19:09:04 +0900

    [6/7] Practice Chapter 7. Ensemble training
    * Add source code of ensemble learning
    * AdaBoosting, GradientBoosting, Feature importance
    * Disable flag as plt.show()
    
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- ensemble_learning.py
+++ ensemble_learning.py
@@ -1,16 +1,20 @@
 import numpy as np
 from matplotlib.colors import ListedColormap
+from sklearn.datasets import fetch_openml
 from sklearn.datasets import load_iris
 from sklearn.datasets import make_moons
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
 
 from settings import *
 
@@ -56,13 +60,13 @@ bag_clf = BaggingClassifier(
     max_samples=100, bootstrap=True, random_state=42)
 bag_clf.fit(X_train, y_train)
 y_pred = bag_clf.predict(X_test)
-print("accuracy_score(y_test, y_pred) of bag_clf\n", accuracy_score(y_test, y_pred))
+print("accuracy_score(y_test, y_pred) of bag_clf:", accuracy_score(y_test, y_pred))
 
 # Train one decision tree
 tree_clf = DecisionTreeClassifier(random_state=42)
 tree_clf.fit(X_train, y_train)
 y_pred_tree = tree_clf.predict(X_test)
-print("accuracy_score(y_test, y_pred) of tree_clf\n", accuracy_score(y_test, y_pred_tree))
+print("accuracy_score(y_test, y_pred) of tree_clf:", accuracy_score(y_test, y_pred_tree))
 
 
 def plot_decision_boundary(clf, X, y, axes=[-1.5, 2.45, -1, 1.5], alpha=0.5, contour=True):
@@ -92,7 +96,7 @@ plot_decision_boundary(bag_clf, X, y)
 plt.title("Decision Trees with Bagging", fontsize=14)
 plt.ylabel("")
 save_fig("decision_tree_without_and_with_bagging_plot")
-plt.show()
+# plt.show()
 
 # 7.2.2 Estimating obb
 bag_clf = BaggingClassifier(
@@ -136,6 +140,25 @@ rnd_clf.fit(iris["data"], iris["target"])
 for name, score in zip(iris["feature_names"], rnd_clf.feature_importances_):
     print(name, score)
 
+mnist = fetch_openml('mnist_784', version=1)
+mnist.target = mnist.target.astype(np.uint8)
+
+rnd_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+rnd_clf.fit(mnist["data"], mnist["target"])
+
+# def plot_digit(data):
+#     image = data.reshape(28, 28)
+#     plt.imshow(image, cmap=matplotlib.cm.hot,
+#                interpolation="nearest")
+#     plt.axis("off")
+#
+#
+# plot_digit(rnd_clf.feature_importances_)
+# cbar = plt.colorbar(ticks=[rnd_clf.feature_importances_.min(), rnd_clf.feature_importances_.max()])
+# cbar.ax.set_yticklabels(['Not important', 'Very important'])
+# save_fig("mnist_feature_importance_plot")
+# plt.show()
+
 # 7.5 Boosting
 # 7.5.1 Adaboost
 ada_clf = AdaBoostClassifier(
@@ -143,19 +166,181 @@ ada_clf = AdaBoostClassifier(
     algorithm="SAMME.R", learning_rate=0.5)
 ada_clf.fit(X_train, y_train)
 
-# # 7.5.2 Gradient Boosting
-#
-# tree_reg1 = DecisionTreeRegressor(max_depth=2)
-# tree_reg1.fit(X, y)
-#
-# y2 = y - tree_reg1.predict(X)
-# tree_reg2 = DecisionTreeRegressor(max_depth=2)
-# tree_reg2.fit(X, y2)
-#
-# y3 = y2 - tree_reg2.predict(X)
-# tree_reg3 = DecisionTreeRegressor(max_depth=2)
-# tree_reg3.fit(X, y3)
-#
-# X_new = np.array([[0.8]])
-# y_pred = sum(tree.predict(X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
-# print(y_pred)
+plot_decision_boundary(ada_clf, X, y)
+
+m = len(X_train)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+for subplot, learning_rate in ((0, 1), (1, 0.5)):
+    sample_weights = np.ones(m) / m
+    plt.sca(axes[subplot])
+    for i in range(5):
+        svm_clf = SVC(kernel="rbf", C=0.2, gamma=0.6, random_state=42)
+        svm_clf.fit(X_train, y_train, sample_weight=sample_weights * m)
+        y_pred = svm_clf.predict(X_train)
+        r = sample_weights[y_pred != y_train].sum() / sample_weights.sum()  # equation 7-1
+        alpha = learning_rate * np.log((1 - r) / r)  # equation 7-2
+        sample_weights[y_pred != y_train] *= np.exp(alpha)  # equation 7-3
+        sample_weights /= sample_weights.sum()  # normalization step
+        plot_decision_boundary(svm_clf, X, y, alpha=0.2)
+        plt.title("learning_rate = {}".format(learning_rate), fontsize=16)
+    if subplot == 0:
+        plt.text(-0.7, -0.65, "1", fontsize=14)
+        plt.text(-0.6, -0.10, "2", fontsize=14)
+        plt.text(-0.5, 0.10, "3", fontsize=14)
+        plt.text(-0.4, 0.55, "4", fontsize=14)
+        plt.text(-0.3, 0.90, "5", fontsize=14)
+    else:
+        plt.ylabel("")
+
+save_fig("boosting_plot")
+
+# 7.5.2 Gradient Boosting
+
+np.random.seed(42)
+X = np.random.rand(100, 1) - 0.5
+y = 3 * X[:, 0] ** 2 + 0.05 * np.random.randn(100)
+
+tree_reg1 = DecisionTreeRegressor(max_depth=2)
+tree_reg1.fit(X, y)
+
+y2 = y - tree_reg1.predict(X)
+tree_reg2 = DecisionTreeRegressor(max_depth=2)
+tree_reg2.fit(X, y2)
+
+y3 = y2 - tree_reg2.predict(X)
+tree_reg3 = DecisionTreeRegressor(max_depth=2)
+tree_reg3.fit(X, y3)
+
+X_new = np.array([[0.8]])
+y_pred = sum(tree.predict(X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
+print("y_pred\n", y_pred)
+
+
+def plot_predictions(regressors, X, y, axes, label=None, style="r-", data_style="b.", data_label=None):
+    x1 = np.linspace(axes[0], axes[1], 500)
+    y_pred = sum(regressor.predict(x1.reshape(-1, 1)) for regressor in regressors)
+    plt.plot(X[:, 0], y, data_style, label=data_label)
+    plt.plot(x1, y_pred, style, linewidth=2, label=label)
+    if label or data_label:
+        plt.legend(loc="upper center", fontsize=16)
+    plt.axis(axes)
+
+
+plt.figure(figsize=(11, 11))
+
+plt.subplot(321)
+plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h_1(x_1)$", style="g-",
+                 data_label="Training set")
+plt.ylabel("$y$", fontsize=16, rotation=0)
+plt.title("Residuals and tree predictions", fontsize=16)
+
+plt.subplot(322)
+plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1)$", data_label="Training set")
+plt.ylabel("$y$", fontsize=16, rotation=0)
+plt.title("Ensemble predictions", fontsize=16)
+
+plt.subplot(323)
+plot_predictions([tree_reg2], X, y2, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_2(x_1)$", style="g-", data_style="k+",
+                 data_label="Residuals")
+plt.ylabel("$y - h_1(x_1)$", fontsize=16)
+
+plt.subplot(324)
+plot_predictions([tree_reg1, tree_reg2], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1) + h_2(x_1)$")
+plt.ylabel("$y$", fontsize=16, rotation=0)
+
+plt.subplot(325)
+plot_predictions([tree_reg3], X, y3, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_3(x_1)$", style="g-", data_style="k+")
+plt.ylabel("$y - h_1(x_1) - h_2(x_1)$", fontsize=16)
+plt.xlabel("$x_1$", fontsize=16)
+
+plt.subplot(326)
+plot_predictions([tree_reg1, tree_reg2, tree_reg3], X, y, axes=[-0.5, 0.5, -0.1, 0.8],
+                 label="$h(x_1) = h_1(x_1) + h_2(x_1) + h_3(x_1)$")
+plt.xlabel("$x_1$", fontsize=16)
+plt.ylabel("$y$", fontsize=16, rotation=0)
+
+save_fig("gradient_boosting_plot")
+# plt.show()
+
+# Left
+gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=3, learning_rate=1.0, random_state=42)
+gbrt.fit(X, y)
+
+# Right
+gbrt_slow = GradientBoostingRegressor(max_depth=2, n_estimators=500, learning_rate=1.0, random_state=42)
+gbrt_slow.fit(X, y)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+
+plt.sca(axes[0])
+plot_predictions([gbrt], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="Ensemble predictions")
+plt.title("learning_rate={}, n_estimators={}".format(gbrt.learning_rate, gbrt.n_estimators), fontsize=14)
+plt.xlabel("$x_1$", fontsize=16)
+plt.ylabel("$y$", fontsize=16, rotation=0)
+
+plt.sca(axes[1])
+plot_predictions([gbrt_slow], X, y, axes=[-0.5, 0.5, -0.1, 0.8])
+plt.title("learning_rate={}, n_estimators={}".format(gbrt_slow.learning_rate, gbrt_slow.n_estimators), fontsize=14)
+plt.xlabel("$x_1$", fontsize=16)
+
+save_fig("gbrt_learning_rate_plot")
+# plt.show()
+
+# Gradient Boosting with early stopping
+X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=49)
+
+gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=120, random_state=42)
+gbrt.fit(X_train, y_train)
+
+errors = [mean_squared_error(y_val, y_pred)
+          for y_pred in gbrt.staged_predict(X_val)]
+bst_n_estimators = np.argmin(errors) + 1
+
+gbrt_best = GradientBoostingRegressor(max_depth=2, n_estimators=bst_n_estimators, random_state=42)
+gbrt_best.fit(X_train, y_train)
+
+min_error = np.min(errors)
+
+plt.figure(figsize=(10, 4))
+
+plt.subplot(121)
+plt.plot(np.arange(1, len(errors) + 1), errors, "b.-")
+plt.plot([bst_n_estimators, bst_n_estimators], [0, min_error], "k--")
+plt.plot([0, 120], [min_error, min_error], "k--")
+plt.plot(bst_n_estimators, min_error, "ko")
+plt.text(bst_n_estimators, min_error * 1.2, "Minimum", ha="center", fontsize=14)
+plt.axis([0, 120, 0, 0.01])
+plt.xlabel("Number of trees")
+plt.ylabel("Error", fontsize=16)
+plt.title("Validation error", fontsize=14)
+
+plt.subplot(122)
+plot_predictions([gbrt_best], X, y, axes=[-0.5, 0.5, -0.1, 0.8])
+plt.title("Best model (%d trees)" % bst_n_estimators, fontsize=14)
+plt.ylabel("$y$", fontsize=16, rotation=0)
+plt.xlabel("$x_1$", fontsize=16)
+
+save_fig("early_stopping_gbrt_plot")
+# plt.show()
+
+# Early stopping with some grace (stop training only when not improving for 5 epochs):
+gbrt = GradientBoostingRegressor(max_depth=2, warm_start=True, random_state=42)
+
+min_val_error = float("inf")
+error_going_up = 0
+for n_estimators in range(1, 120):
+    gbrt.n_estimators = n_estimators
+    gbrt.fit(X_train, y_train)
+    y_pred = gbrt.predict(X_val)
+    val_error = mean_squared_error(y_val, y_pred)
+    if val_error < min_val_error:
+        min_val_error = val_error
+        error_going_up = 0
+    else:
+        error_going_up += 1
+        if error_going_up == 5:
+            break  # early stopping
+
+print("gbrt.n_estimators:", gbrt.n_estimators)
+print("Minimum validation MSE:", min_val_error)
--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ os.makedirs(IMAGES_PATH, exist_ok=True)
 
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
-    print("그림 저장:", fig_id)
+    print("Saving figure:", fig_id)
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
